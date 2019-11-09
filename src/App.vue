5ab332bf2d29d41d5a8345df42128ddb89306922
Rohan van Wijk <580626@student.inholland.nl>
2019-11-09 23:13:26 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Hero HeroTitle="Rohan Photo" imageSource="https://dummyimage.com/1200x900/000/fff" />
+  </div>
+</template>
+
+<script>
+import Hero from './components/Hero.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Hero
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hero">
+        <h1>{{ HeroTitle }}</h1>
+        <img :src="imageSource" alt="Image" />
+    </div>
+</template>
+<script>
+export default {
+    name: 'Hero',
+    props: {
+        HeroTitle: String,
+        imageSource: String,
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+.hero {
+    padding: 0;
+}
+
+</style>
