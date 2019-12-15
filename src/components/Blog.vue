6bd572384fec04ffc940e865a79e270a41616b9e
Rohan van Wijk <rohan_10@live.nl>
2019-12-15 11:22:21 +0100

    empty default src.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -31,7 +31,7 @@ export default {
     data: function() {
         return {
             isShowedUp: false,
-            showFullSrc: 'koek'
+            showFullSrc: ''
         }
     },
     computed: {
